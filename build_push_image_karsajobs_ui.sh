f5fdb275e271cfa031e00f053b88aadef817a724
isutriadi <101785099+isutriadi@users.noreply.github.com>
2023-02-18 18:41:40 +0800

    Update build_push_image_karsajobs_ui.sh

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -12,5 +12,8 @@ docker build -t $GH_REGISTRY_IMAGE .
 # mengubah tag
 docker tag localhost/$GH_REGISTRY_IMAGE $GH_REGISTRY_REPO/$GH_REGISTRY_IMAGE
 
+# melakukan login
+echo $PASSWORD_DOCKER_HUB | docker login GH_REGISTRY -u GH_REGISTRY_LOGIN --password-stdin
+
 # mengunggah image
 docker push $GH_REGISTRY_REPO/$GH_REGISTRY_IMAGE
